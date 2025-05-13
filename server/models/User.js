337ac91115ac1435e41055fcc9dfea7d235be1de
AliCapone21 <farxodovali5@gmail.com>
2025-05-13 17:10:14 -0400

    Latest update

--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    clerkId: { type: String, required: true, unique: true }, // ✅ Clerk ID
+    _id: { type: String, required: true },
     name: { type: String, required: true },
     email: { type: String, required: true },
     imageUrl: { type: String, required: true },
@@ -16,6 +16,7 @@ const userSchema = new mongoose.Schema({
     }
 }, { timestamps: true });
 
+
 const User = mongoose.model("User", userSchema);
 
-export default User;
+export default User
